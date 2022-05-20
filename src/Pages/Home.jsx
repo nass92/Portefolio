d75a11df462d3583fb24d4facbd48ea353beaa0c
Nassim G <goujanassim@hotmail.com>
2022-05-20 03:51:54 +0200

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,11 +3,32 @@ import { Center, Heading} from '@chakra-ui/react';
 import '../Styles/Pages/Home.css'
 import NavBar from "../Components/Navbar";
 import GridTemplate from "../Components/Welcom";
-
+import { motion } from "framer-motion";
 const Home = () => {
-
+    const variants = {
+        initial: {
+          opacity: 0,
+          transition: { duration: 0.5 },
+          x: 100,
+        },
+        visible: {
+          opacity: 1,
+          x: 0,
+        },
+        exit: {
+          opacity: 0,
+          transition: { duration: 0.3 },
+          x: -100,
+        }
+      };
     return (
-      
+        <motion.div
+        className="home"
+        initial="initial"
+        animate="visible"
+        exit="exit"
+        variants={variants}
+      >
      <div id="home">
   
    
@@ -19,6 +40,7 @@ const Home = () => {
  
   
             </div>
+            </motion.div>
     );
 }
 
